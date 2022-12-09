452ce9a0206a468c8a88eac151f938623d3ee53e
Junliang Huang <junhuang@umich.edu>
2022-12-08 19:55:35 -0500

    Fix script.

--- run_greedy.sh
+++ run_greedy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # -n=10 -e=0.5
-python3 driver.py
 python3 driver.py tests/sum-to-n/ -m=greedy
 python3 driver.py tests/alloc-loop/ -m=greedy
 python3 driver.py tests/matrix_multiply/ -m=greedy
